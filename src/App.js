b629e03e9f41f008559bd2d767adfb24990a3dfd
jandersonmoura <janderson-moura@hotmail.com>
2022-12-15 18:48:01 -0200

    seccon-project-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { Container, Image, ContainerItens, H1, Inputlabel, Input, Button, User } from './styles';
 
-function App() {
+import People from './assets/people.png';
+import Arrow from './assets/arrow.png';
+import Trash from './assets/trash.jpg';
+
+
+const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef()
+
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }])
+  }
+
+  function deleteUser(userId){
+    console.log(userId)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Image alt='logo-image' src={People} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+
+        <Inputlabel>Nome</Inputlabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <Inputlabel>Idade</Inputlabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewUser}>Cadastrar <img alt='seta' src={Arrow} /></Button>
+
+        <ul>
+          {users.map(user => (
+            <User key={user.id}>
+              <p>{user.name}</p>  <p>{user.age}</p>
+              <button onClick={deleteUser(user.id)}>
+                <img alt='lata-de-lixo' src={Trash} />
+              </button>
+            </User>
+          ))
+
+          }
+        </ul>
+
+      </ContainerItens>
+    </Container>
+  )
 }
 
 export default App;
